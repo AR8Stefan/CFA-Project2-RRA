c7f623160b572fdae96176f6e74166cf2be8fddf
Stefan <stefan.piatkov@gmail.com>
2017-04-23 18:04:52 +1000

    Add styles, fix little bugs. And backup

--- app/models/refugee.rb
+++ app/models/refugee.rb
@@ -1,9 +1,10 @@
 class Refugee < ApplicationRecord
+	
 	resourcify
 	belongs_to :user
 	mount_uploader :image, ImageUploader
 
 	def self.search(search)
-  		where("name ILIKE ? OR occupation ILIKE ? OR contactNumber ILIKE ? OR language ILIKE ? OR description ILIKE", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%") 
+  		where("name ILIKE ? OR occupation ILIKE ? OR contactNumber ILIKE ? OR language ILIKE ? OR description ILIKE ? ", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}") 
 	end
 end
